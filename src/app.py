9bdec10136147a42b1c1d921e2c7589d5b54bc1b
Santiago Diaz <santiagodiaz9822@gmail.com>
2025-03-03 14:49:58 -0300

    fix cmd comand dockerfile

--- src/app.py
+++ src/app.py
@@ -1,19 +1,27 @@
 from fastapi import FastAPI
 from langserve import add_routes
-from my_rag_pipeline import rag_chain  # Import your RAG chain
+from rag_pipeline import rag_chain  # Ensure this import is correct
+import uvicorn
+import argparse
 
 app = FastAPI(
-    title="Promtior Chatbot API",
-    description="API for Promtior's RAG-powered chatbot using Llama2 and Ollama",
+    title="Promitor Chatbot API",
+    description="API for Promitor's RAG-powered chatbot using Llama2 and Ollama",
 )
 
 # Add the RAG chain as an endpoint
 add_routes(
     app,
     rag_chain,
-    path="/promtior-chatbot",
+    path="/promitor-chatbot",
 )
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="0.0.0.0", help="Host address")
+    parser.add_argument("--port", type=int, default=8000, help="Port number")
+    args = parser.parse_args()
+
+    # Start the Uvicorn server
+    uvicorn.run("app:app", host=args.host, port=args.port, reload=True)
